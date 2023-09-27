9fecb88b5f1e3f6ecbe6cd4105400fa05fa05211
yuvraj0028 <yuvraj1602.be21@chitkara.edu.in>
2023-09-28 00:07:15 +0530

    Added page trasition animations

---
+++ lib/utils/page_animation.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PageAnimation {
+  static Route createRoute(
+      {required Widget page,
+      required double beginOffset1,
+      required double beginOffset2}) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final begin = Offset(beginOffset1, beginOffset2);
+        const end = Offset.zero;
+
+        const curve = Curves.easeIn;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+}
